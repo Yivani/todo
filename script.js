f4f03871b6930f8a4a9b73d72edfeed3d3011162
Yivani <83837074+Yivani@users.noreply.github.com>
2023-12-01 13:39:07 +0100

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// In script.js
-
-// Laden von gespeicherten Einträgen beim Start der Seite
-document.addEventListener("DOMContentLoaded", loadFromLocalStorage);
-
 function addTask() {
   const taskInput = document.getElementById("task-input");
   const taskText = taskInput.value.trim();
 
   if (taskText !== "") {
     const todoList = document.getElementById("todo-list");
-
+    
     const listItem = document.createElement("li");
     listItem.innerHTML = `
       <span>${taskText}</span>
@@ -18,9 +13,6 @@ function addTask() {
 
     todoList.appendChild(listItem);
     taskInput.value = "";
-
-    // Speichern in localStorage
-    saveToLocalStorage();
   }
 }
 
@@ -28,39 +20,4 @@ function removeTask(button) {
   const listItem = button.parentNode;
   const todoList = document.getElementById("todo-list");
   todoList.removeChild(listItem);
-
-  // Aktualisieren von localStorage nach dem Entfernen
-  saveToLocalStorage();
-}
-
-function saveToLocalStorage() {
-  const todoList = document.getElementById("todo-list");
-  const tasks = [];
-
-  // Durchlaufe alle Einträge und speichere den Text
-  todoList.querySelectorAll("li span").forEach((task) => {
-    tasks.push(task.textContent);
-  });
-
-  // Speichern im localStorage
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function loadFromLocalStorage() {
-  const todoList = document.getElementById("todo-list");
-  const savedTasks = localStorage.getItem("tasks");
-
-  if (savedTasks) {
-    const tasks = JSON.parse(savedTasks);
-
-    tasks.forEach((taskText) => {
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `
-        <span>${taskText}</span>
-        <button onclick="removeTask(this)">Remove</button>
-      `;
-
-      todoList.appendChild(listItem);
-    });
-  }
 }
